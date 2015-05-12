2d989053e70ea9312c16c5ecf4e0639a546789e3
Collin Kidder <collink@kkmfg.com>
2015-05-12 18:47:26 -0400

    Removed the *.user file from the index. It doesn't belong being version tracked. Also fixed an extraneous qualifier that didn't mess up on Windows but breaks the compile on OSX.

--- canframemodel.h
+++ canframemodel.h
@@ -16,7 +16,7 @@ public:
     QVariant data(const QModelIndex &index, int role) const;
     QVariant headerData(int section, Qt::Orientation orientation,
                         int role = Qt::DisplayRole) const;
-    int CANFrameModel::columnCount(const QModelIndex &) const;
+    int columnCount(const QModelIndex &) const;
 
     void addFrame(CANFrame &, bool);
     void sendRefresh();
