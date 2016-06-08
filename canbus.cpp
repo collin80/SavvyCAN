38d3b348323d5162e66e77a0131e648b9615a0ea
canpadawan <canpadawan@users.noreply.github.com>
2016-06-08 11:31:49 +0200

    mv canbus to its own file

--- canbus.cpp
+++ canbus.cpp
@@ -1,6 +1,81 @@
+#include <QObject>
 #include "canbus.h"
 
-CAN_Bus::CAN_Bus()
+CANBus::CANBus()
 {
+    speed = 250000;
+    listenOnly = false;
+    singleWire = false;
+    active = false;
+    container = Q_NULLPTR;
+    busNum = 0;
+}
 
+CANBus::CANBus(const CANBus& pBus) :
+    speed(pBus.speed),
+    listenOnly(pBus.listenOnly),
+    singleWire(pBus.singleWire),
+    active(pBus.active),
+    container(pBus.container), /* TODO: check if container is really needed */
+    busNum(pBus.busNum) {}
+
+
+void CANBus::setSpeed(int newSpeed)
+{
+    speed = newSpeed;
+}
+
+void CANBus::setListenOnly(bool mode)
+{
+    listenOnly = mode;
+}
+
+void CANBus::setSingleWire(bool mode)
+{
+    singleWire = mode;
+}
+
+void CANBus::setEnabled(bool mode)
+{
+    active = mode;
+}
+
+void CANBus::setContainer(CANConnectionContainer* pContainer)
+{
+    container = pContainer;
+}
+
+void CANBus::setBusNum(int num)
+{
+    busNum = num;
+}
+
+int CANBus::getSpeed()
+{
+    return speed;
+}
+
+int CANBus::getBusNum()
+{
+    return busNum;
+}
+
+bool CANBus::isListenOnly()
+{
+    return listenOnly;
+}
+
+bool CANBus::isSingleWire()
+{
+    return singleWire;
+}
+
+bool CANBus::isActive()
+{
+    return active;
+}
+
+CANConnectionContainer* CANBus::getContainer()
+{
+    return container;
 }
--- canbus.h
+++ canbus.h
@@ -1,11 +1,34 @@
-#ifndef CAN_BUS_H
-#define CAN_BUS_H
+#ifndef CANBus_H
+#define CANBus_H
 
 
-class CAN_Bus
+class CANConnectionContainer;
+
+class CANBus
 {
 public:
-    CAN_Bus();
+    CANBus();
+    CANBus(const CANBus&);
+    virtual ~CANBus(){}; /*TODO: remove connection from CANBus and add CANBus as an element of CANConnection */
+    int busNum;
+    int speed;
+    bool listenOnly;
+    bool singleWire;
+    bool active; //is this bus turned on?
+    CANConnectionContainer* container;
+
+    void setSpeed(int); // new speed
+    void setListenOnly(bool); //bool for whether to only listen
+    void setSingleWire(bool); //bool for whether to use single wire mode
+    void setEnabled(bool); //whether this bus should be enabled or not.
+    void setContainer(CANConnectionContainer *);
+    CANConnectionContainer* getContainer();
+    void setBusNum(int);
+    int getSpeed();
+    int getBusNum();
+    bool isListenOnly();
+    bool isSingleWire();
+    bool isActive();
 };
 
-#endif // CAN_BUS_H
+#endif // CANBus_H
