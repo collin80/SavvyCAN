db7ad421990a8e236f2cdbb3d2fe2a93808edbfc
canpadawan <canpadawan@users.noreply.github.com>
2016-06-29 11:54:36 +0200

    add << and >> for serialization

---
+++ connections/canconmanager.cpp
@@ -0,0 +1,6 @@
+#include "canconmanager.h"
+
+CANConManager::CANConManager()
+{
+
+}
---
+++ connections/canconmanager.h
@@ -0,0 +1,11 @@
+#ifndef CANCONMANAGER_H
+#define CANCONMANAGER_H
+
+
+class CANConManager
+{
+public:
+    CANConManager();
+};
+
+#endif // CANCONMANAGER_H
