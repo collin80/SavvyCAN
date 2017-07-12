2764c9bc52b74b494f2665175f7c73a5894fa475
Collin Kidder <collink@kkmfg.com>
2017-07-12 18:34:54 -0400

    Change GUI refresh rate to 4 times per second instead of 2. The refresh rate is purposefully limited to allow for better system performance while capturing but 4 times per second is not really too fast. Also updated version number

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG
 #define CONFIG
 
-#define VERSION             170
+#define VERSION             171
 
 //try to keep this in sync.
 //SavvyCAN will complain if you connect a GVRET board with a revision
