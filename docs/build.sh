b4b87afee3610337f519615426e18f8fd22a0fd8
Collin Kidder <collink@kkmfg.com>
2019-02-20 21:51:31 -0500

    Update documentation to match the new features.

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,4 @@
 make qthelp
-~/Qt/5.11.2/gcc_64/bin/qcollectiongenerator build/qthelp/SavvyCAN.qhcp
-~/Qt/5.11.2/gcc_64/bin/assistant -collectionFile build/qthelp/SavvyCAN.qhc
+~/Qt/5.12.1/gcc_64/bin/qcollectiongenerator build/qthelp/SavvyCAN.qhcp
+~/Qt/5.12.1/gcc_64/bin/assistant -collectionFile build/qthelp/SavvyCAN.qhc
 
