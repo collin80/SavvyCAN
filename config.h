ba94db77bf2dba10692320a632016e26d4a6bc0e
Collin Kidder <collink@kkmfg.com>
2019-03-19 20:43:36 -0400

    Update version number, small fix to graphing window

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG
 #define CONFIG
 
-#define VERSION             195
+#define VERSION             196
 
 //try to keep this in sync.
 //SavvyCAN will complain if you connect a GVRET board with a revision
