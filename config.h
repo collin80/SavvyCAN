5d52f5a37e5306ba3f429bc7d03fa7304f64ce76
Collin Kidder <collink@kkmfg.com>
2021-08-25 19:26:35 -0400

    VERY belated increase in version number.
    Added build date to main window title to make it easier to
    differentiate continuous builds from each other.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG
 #define CONFIG
 
-#define VERSION             204
+#define VERSION             206
 
 //try to keep this in sync.
 //SavvyCAN will complain if you connect a GVRET board with a revision
