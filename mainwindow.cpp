49e21e28eb845920d7cb46888c87bdc7d5eaa438
Ludovic LANGE <llange@users.noreply.github.com>
2022-09-24 17:51:25 +0200

    remove qRegisterMetaTypeStreamOperators (qt6)
    Since Qt6, some methods of [QMetaType are removed](https://doc.qt.io/qt-6/qtcore-changes-qt6.html#the-qmetatype-class).
    
    We remove `qRegisterMetaTypeStreamOperators()` calls for Qt >= 6

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -30,8 +30,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+#if QT_VERSION < QT_VERSION_CHECK( 6, 0, 0 )
     qRegisterMetaTypeStreamOperators<QVector<QString>>();
     qRegisterMetaTypeStreamOperators<QVector<int>>();
+#endif
 
     useHex = true;
 
