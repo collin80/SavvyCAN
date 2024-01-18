3e85c74cfa9473fa576298019cb90a7554654933
Alexey Esaulenko <esaulenka@gmail.com>
2024-01-18 12:52:25 +0400

    Minor: Utility::formatTimestamp() suppress compiler warning

--- utility.h
+++ utility.h
@@ -190,6 +190,7 @@ public:
             return (double)timestamp / 1000000.0;
             break;
         }
+        return QVariant();  // should never happen
     }
 
     //parses the input string to grab as much of it as possible while staying alpha numeric
